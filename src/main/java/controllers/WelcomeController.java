a0371404b21ede8c240b8dbaeed4a7eeca2be519
marta <marta.voitanowych@gmail.com>
2017-04-03 21:23:29 +0300

    add dependencies add plugin in build create controller, index.jsp,web.xml, action-servlet

---
+++ src/main/java/controllers/WelcomeController.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Марта on 03.04.2017.
+ */
+
+@Controller
+public class WelcomeController {
+
+    private int visitorCount = 0;
+
+    @RequestMapping("/index.html")
+    public String index(Model model) {
+        model.addAttribute("visitorCount", visitorCount++);
+        return "WEB-INF/jsp/index.jsp";
+    }
+
+}
